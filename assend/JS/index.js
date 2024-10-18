062c39f10e8b385a1ce7c713a3aaaa7e02472e8a
Lynn Meza <u20201c320@upc.edu.pe>
2024-10-17 21:09:37 -0500

    feat: update content

---
+++ assend/JS/index.js
@@ -0,0 +1,41 @@
+document.body.classList.add('bg-red');
+document.body.classList.remove('one');
+
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('change', () => {
+	body.classList.toggle('dark-mode');
+});
+
+console.log(document.body.classList);
+
+// querySelector
+const btnOpen = document.querySelector('.btn--open');
+const btnClose = document.querySelector('.btn--close');
+const navMenu = document.querySelector('.nav__menu');
+console.log(btnOpen);
+
+// Agregar evento por propiedad
+btnOpen.onclick = function () {
+	// navMenu.classList.add('show--menu')
+	navMenu.classList.toggle('show--menu');
+};
+
+btnClose.onclick = function () {
+	navMenu.classList.remove('show--menu');
+};
+const links = document.querySelectorAll('.nav__link');
+
+for (const link of links) {
+	link.addEventListener('click', function () {
+		navMenu.classList.remove('show--menu');
+	});
+}
+
+var typed = new Typed('#typing', {
+	strings: ['Frontend', 'Backend'],
+	typeSpeed: 50,
+	loop: true,
+	cursorChar: '_',
+});
